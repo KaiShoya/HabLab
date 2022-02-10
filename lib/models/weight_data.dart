66d4c58e53b27dd2b60f77caf8fa52034fb7199f
Kai Shoya <kai@co-well.jp>
2022-02-10 21:59:34 +0900

    WeightDataを利用してグラフ・リスト表示を更新できるようにした

--- lib/models/weight_data.dart
+++ lib/models/weight_data.dart
@@ -1,6 +1,37 @@
+import 'package:intl/intl.dart';
+
+const String tableWeightData = 'weight_data';
+const String columnId = 'id';
+const String columnDate = 'date';
+const String columnWeight = 'weight';
+
 class WeightData {
-  final DateTime date;
-  final double weight;
+  int? id;
+  DateTime date;
+  double weight;
+
+  WeightData(this.id, this.date, this.weight);
+
+  String formatString() {
+    DateFormat outputFormat = DateFormat('yyyy-MM-dd');
+    return outputFormat.format(date) + " " + weight.toString();
+  }
+
+  Map<String, Object?> toMap() {
+    var map = <String, Object?>{columnDate: date, columnWeight: weight};
+    return map;
+  }
+}
+
+class WeightDataList {
+  List<WeightData> list = [];
+  WeightDataList();
+
+  void add(WeightData data) {
+    list.add(data);
+  }
+}
+
 
   WeightData(this.date, this.weight);
 }
--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,8 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../components/listview.dart';
+import '../components/chart.dart';
+import '../models/weight_data.dart';
 
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
   const Page1({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  final WeightDataList _weightList = WeightDataList();
+  DateTime date = DateTime(2022, 1, 1);
+  _Page1State() {
+    _weightList.add(WeightData(null, date, 40));
+    date = date.add(const Duration(days: 1));
+    _weightList.add(WeightData(null, date, 53));
+    date = date.add(const Duration(days: 1));
+    _weightList.add(WeightData(null, date, 50));
+    date = date.add(const Duration(days: 1));
+    _weightList.add(WeightData(null, date, 40));
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      date = date.add(const Duration(days: 1));
+      _weightList
+          .add(WeightData(null, date, Random.secure().nextDouble() * 100));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +39,18 @@ class Page1 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('1'),
       ),
-      body: const Center(
-        child: ListViewWidget(items: [
-          'items1',
-          'items2',
-          'items3',
-          'items4',
-          'items5',
-          'items6',
-          'items7',
-          'items8',
-          'items9',
-          'items10',
-        ]),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+        SizedBox(height: 300, child: ChartWidget(weightList: _weightList.list)),
+        Expanded(
+            child: ListViewWidget(
+                items: _weightList.list.map((e) => e.formatString()).toList())),
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
