9f63cb66afd87cfefd0add9e92d61d701bd037a9
Kai Shoya <kai@co-well.jp>
2022-02-15 02:15:54 +0900

    table_calendarを導入した

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/material.dart';
-import '../components/chart.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../models/amount_data.dart';
+import '../utils.dart';
 
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
   const Page2({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  static final AmountDataList _amountList = AmountDataList();
+  AmountDataProvider provider = AmountDataProvider();
+  DateTime date = DateTime(2022, 1, 1);
+  static DateTime _selectedDay = DateTime.now();
+  static DateTime _focusedDay = DateTime.now();
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  _Page2State() {
+    provider.get1Month(2022, 1).then((data) => _amountList.update(data));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<AmountData> amountList = [];
-    AmountDataProvider().open().then((provider) =>
-        provider.get1Month(2022, 1).then((data) => amountList = data));
     return Scaffold(
       appBar: AppBar(
         title: const Text('2'),
       ),
       body: Center(
-        child: ChartWidget(amountList: amountList),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              firstDay: DateTime.utc(2022, 1, 1),
+              lastDay: DateTime.now().add(const Duration(days: 365)),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              eventLoader: _getEventsForDay,
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            // Expanded(child: ListViewAmountDataWidget(items: _amountList)),
+          ],
+        ),
       ),
     );
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return kEvents[day] ?? [];
   }
 }
